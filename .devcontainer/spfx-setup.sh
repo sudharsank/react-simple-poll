e470cc9a0ae234a3b887d2371cfcee2d429a84b9
Sudharsan Kesavanarayanan <sudharsan_1985@live.in>
2020-10-16 18:07:53 +0800

    Add files via upload

---
+++ .devcontainer/spfx-setup.sh
@@ -0,0 +1,9 @@
+. ${NVM_DIR}/nvm.sh
+npm config delete prefix
+nvm install 10
+nvm use 10.22.1
+npm install -g yo gulp
+npm install -g @microsoft/generator-sharepoint
+npm install
+npm rebuild node-sass
+npm config delete prefix
